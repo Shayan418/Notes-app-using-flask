8c811a284a0748dbb254f534b4a5c55cb21f7d24
jahanzebshayan <jahanzebshayan14@gmail.com>
2021-11-04 16:02:54 +0530

    java script bug fix

--- static/script.js
+++ static/script.js
@@ -1,8 +1,10 @@
 window.onload = function () {
     function calcHeight(value) {
         let numberOfLineBreaks = (value.match(/\n/g) || []).length;
+        if (numberOfLineBreaks < 8)
+            numberOfLineBreaks = 8;
         // min-height + lines x line-height + padding + border
-        let newHeight = 24 + numberOfLineBreaks * 24 + 13;
+        let newHeight = 24 + numberOfLineBreaks * 24 + 12 + 0.998;
         return newHeight;
     }
     let textarea = document.getElementById('resize-ta');
